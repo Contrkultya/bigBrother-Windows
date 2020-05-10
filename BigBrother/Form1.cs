bad10afa96fecdcabc84af0e2298107a76ccbd28
Sergo Skeletonson <40607257+Contrkultya@users.noreply.github.com>
2020-05-10 15:03:04 +0500

    Добавьте файлы проекта.

---
+++ BigBrother/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+
+namespace BigBrother
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            // Configure color schema
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Red400, Primary.Red500,
+                Primary.Red500, Accent.Red200,
+                TextShade.WHITE
+            );
+
+        }
+    }
+}
